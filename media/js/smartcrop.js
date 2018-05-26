3a49661c8ce1f3314b9909bb968e4295207b4787
Geetanshu <mathur.geetanshu887@gmail.com>
2018-05-26 23:54:05 +0530

    updated js file

--- media/js/smartcrop.js
+++ media/js/smartcrop.js
@@ -13,11 +13,11 @@ Joomla.MediaManager.Edit = Joomla.MediaManager.Edit || {};
 		var image = document.getElementById('image-preview');
 		
 		// Initiate the cropper for gathering the focus point
-		Joomla.MediaManager.Edit.smartcrop = new Cropper(image, {
+		Joomla.MediaManager.Edit.smartcrop.cropper = new Cropper(image, { 
 			viewMode: 1,
 			responsive: false,
 			restore: true,
-			autoCrop: false,
+			autoCrop: true,
 			movable: true,
 			zoomable: false,
 			rotatable: false,
@@ -28,16 +28,16 @@ Joomla.MediaManager.Edit = Joomla.MediaManager.Edit || {};
 
 				// Top, Bottom, Left, right are the data focus points
                 var canvas_data = this.cropper.getCropBoxData();
-                var top = canvas_data.top / image.naturalHeight;
-                var bottom = (canvas_data.height + canvas_data.top) / image.naturalHeight;
-                var left = canvas_data.left / image.naturalWidth;
-				var right = (canvas_data.width + canvas_data.left) / image.naturalWidth;
+                Joomla.MediaManager.Edit.smartcrop.cropper.top = (canvas_data.top / image.naturalHeight).toFixed(2);
+                Joomla.MediaManager.Edit.smartcrop.cropper.bottom = ((canvas_data.height + canvas_data.top) / image.naturalHeight).toFixed(2);
+                Joomla.MediaManager.Edit.smartcrop.cropper.left = (canvas_data.left / image.naturalWidth).toFixed(2);
+				Joomla.MediaManager.Edit.smartcrop.cropper.right = ((canvas_data.width + canvas_data.left) / image.naturalWidth).toFixed(2);
 				
 				// Setting the computed focus point into the input fields
-				document.getElementById('jform_data_focus_top').value = top.toFixed(2);
-				document.getElementById('jform_data_focus_bottom').value = bottom.toFixed(2);
-				document.getElementById('jform_data_focus_left').value = left.toFixed(2);
-				document.getElementById('jform_data_focus_right').value = right.toFixed(2);
+				document.getElementById('jform_data_focus_top').value = Joomla.MediaManager.Edit.smartcrop.cropper.top;
+				document.getElementById('jform_data_focus_bottom').value = Joomla.MediaManager.Edit.smartcrop.cropper.bottom;
+				document.getElementById('jform_data_focus_left').value = Joomla.MediaManager.Edit.smartcrop.cropper.left;
+				document.getElementById('jform_data_focus_right').value = Joomla.MediaManager.Edit.smartcrop.cropper.right;
 
 				// Manageing image extension 
 				var format = Joomla.MediaManager.Edit.original.extension === 'jpg' ? 'jpeg' : Joomla.MediaManager.Edit.original.extension;
@@ -48,7 +48,7 @@ Joomla.MediaManager.Edit = Joomla.MediaManager.Edit || {};
 				// Notify the app that a change has been made
 				window.dispatchEvent(new Event('mediaManager.history.point'));
 			}
-        });
+		});
     }
 
     // Register the Events
@@ -58,6 +58,18 @@ Joomla.MediaManager.Edit = Joomla.MediaManager.Edit || {};
 			initSmartCrop(mediaData);
 		},
 		Deactivate: function () {
+			var path = getQueryVariable('path');
+			path = path.split(':');
+			path = '/images' + path[1];
+			var data = "&data-focus-top="+Joomla.MediaManager.Edit.smartcrop.cropper.top+
+					"&data-focus-left="+Joomla.MediaManager.Edit.smartcrop.cropper.left+
+					"&data-focus-bottom="+Joomla.MediaManager.Edit.smartcrop.cropper.bottom+
+					"&data-focus-right="+Joomla.MediaManager.Edit.smartcrop.cropper.right;
+			var xhr = new XMLHttpRequest();
+			var url = window.location.origin+"/adaptive_images/index.php?option=com_content&task=adaptiveimage.setfocus&path="+path;
+			url += data;
+			xhr.open("GET", url, true);
+			xhr.send();
             if (!Joomla.MediaManager.Edit.smartcrop.cropper) {
 				return;
 			}
@@ -65,4 +77,17 @@ Joomla.MediaManager.Edit = Joomla.MediaManager.Edit || {};
 			Joomla.MediaManager.Edit.smartcrop.cropper.destroy();
 		}
 	};
+
+	function getQueryVariable(variable) {
+		var query = window.location.search.substring(1);
+		var vars = query.split('&');
+		for (var i = 0; i < vars.length; i++) {
+			var pair = vars[i].split('=');
+			if (decodeURIComponent(pair[0]) == variable) {
+				return decodeURIComponent(pair[1]);
+			}
+		}
+		return false;
+	};
+
 })();
